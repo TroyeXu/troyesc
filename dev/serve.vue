4aeb74fac7c071eb53a7e6e935fa3b604bf490b4
troye <troye@forfuns.net>
2020-09-26 16:30:36 +0800

    add

---
+++ dev/serve.vue
@@ -0,0 +1,17 @@
+<script>
+import Vue from 'vue';
+import Sc from '@/sc.vue';
+
+export default Vue.extend({
+  name: 'ServeDev',
+  components: {
+    Sc
+  }
+});
+</script>
+
+<template>
+  <div id="app">
+    <sc />
+  </div>
+</template>
